278617dd73be952c3bb42d5f091f251877aa2f22
Alexandre Fayadas <afayadas@202-ecommerce.com>
2022-04-22 17:58:18 +0200

    refs #34616 : add Callback Model and Response classes

---
+++ src/Response/CallbackResponse.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * PHP version 5.6+
+ *
+ * @category  YounitedpaySDK
+ * @package   Ecommerceyounitedpaysdk
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright 2022 (c) 202-ecommerce
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link      https://api.sandbox-younited-pay.com/
+ */
+
+namespace YounitedPaySDK\Response;
+
+use YounitedPaySDK\Model\Callback;
+
+class CallbackResponse extends AbstractResponse
+{
+    /**
+     * @inherit
+     */
+    public function getModel()
+    {
+        $model = new Callback();
+
+        return $model;
+    }
+}
