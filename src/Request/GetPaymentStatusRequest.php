c7a5399eea8b6199f92fe0a03ee81c0126740faa
Alexandre Fayadas <afayadas@202-ecommerce.com>
2025-03-18 15:01:48 +0100

    refs #50992 : add GetPayment, GetPaymentStatus and GetPaymentLink model, request and response

---
+++ src/Request/GetPaymentStatusRequest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * PHP version 5.6+
+ *
+ * @category  YounitedpaySDK
+ * @package   Ecommerceyounitedpaysdk
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright 2022 (c) 202-ecommerce
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link      https://api.sandbox-younited-pay.com/
+ */
+
+namespace YounitedPaySDK\Request;
+
+use InvalidArgumentException;
+use YounitedPaySDK\Model\AbstractModel;
+use YounitedPaySDK\Model\GetPaymentStatus;
+use YounitedPaySDK\Response\GetPaymentStatusResponse;
+
+/**
+ * Get Payment Status Request Class
+ */
+class GetPaymentStatusRequest extends AbstractRequest
+{
+    /**
+     * @var string
+     */
+    protected $requestTarget = '/payments/{id}/status';
+
+    /**
+     * @var string
+     */
+    protected $method = 'GET';
+
+    /** @var string */
+    protected $response = GetPaymentStatusResponse::class;
+
+    /**
+     * @inherit
+     */
+    public function setModel(AbstractModel $body)
+    {
+        if ($body instanceof GetPaymentStatus) {
+            $this->requestTarget = str_replace(
+                '{id}',
+                urlencode($body->getId()),
+                $this->requestTarget
+            );
+            return parent::setModel($body);
+        }
+
+        throw new InvalidArgumentException(
+            'Body must be an instance of ' .  GetPaymentStatus::class . ' ' . get_class($body) . ' given.'
+        );
+    }
+}
