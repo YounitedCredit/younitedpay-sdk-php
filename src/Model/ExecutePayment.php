da11948c30c47061224c6700f14a8fc5edd844bb
Alexandre Fayadas <afayadas@202-ecommerce.com>
2025-03-18 14:37:41 +0100

    refs #50992 : add CancelPayment and ExecutePayment model, request and response

---
+++ src/Model/ExecutePayment.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * PHP version 5.6+
+ *
+ * @category  YounitedpaySDK
+ * @package   Ecommerceyounitedpaysdk
+ * @author    Michael Dowling and contributors to guzzlehttp/psr7
+ * @author    Tobias Nyholm  and contributors to Nyholm/psr7
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright 2022 (c) 202-ecommerce
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link      https://api.sandbox-younited-pay.com/
+ */
+
+namespace YounitedPaySDK\Model;
+
+use InvalidArgumentException;
+use JsonSerializable;
+
+/**
+ * Execute Payment Model Class
+ */
+class ExecutePayment extends AbstractModel implements JsonSerializable
+{
+    // PROPERTIES
+
+    /**
+     * @var string
+     */
+    private $id;
+
+    // GETTERS & SETTERS
+
+    /**
+     * Get Id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Id
+     *
+     * @param string $id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        if (is_string($id) === true) {
+            $this->id = $id;
+            return $this;
+        }
+
+        throw new InvalidArgumentException(
+            'Id must be a string but ' . gettype($id) . ' is given.'
+        );
+    }
+}
---
+++ src/Request/CancelPaymentRequest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * PHP version 5.6+
+ *
+ * @category  YounitedpaySDK
+ * @package   Ecommerceyounitedpaysdk
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright 2022 (c) 202-ecommerce
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link      https://api.sandbox-younited-pay.com/
+ */
+
+namespace YounitedPaySDK\Request;
+
+use InvalidArgumentException;
+use YounitedPaySDK\Model\AbstractModel;
+use YounitedPaySDK\Model\CancelPayment;
+use YounitedPaySDK\Response\CancelPaymentResponse;
+
+/**
+ * Cancel Payment Request Class
+ */
+class CancelPaymentRequest extends AbstractRequest
+{
+    /**
+     * @var string
+     */
+    protected $requestTarget = '/payments/{id}/cancel';
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /** @var string */
+    protected $response = CancelPaymentResponse::class;
+
+    /**
+     * @inherit
+     */
+    public function setModel(AbstractModel $body)
+    {
+        if ($body instanceof CancelPayment) {
+            $this->requestTarget = str_replace(
+                '{id}',
+                urlencode($body->getId()),
+                $this->requestTarget
+            );
+            return parent::setModel($body);
+        }
+
+        throw new InvalidArgumentException(
+            'Body must be an instance of ' .  CancelPayment::class . ' ' . get_class($body) . ' given.'
+        );
+    }
+}
